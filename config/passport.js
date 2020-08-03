64a2399afea320a1866e51c5c91f3fd5020e7967
Satoru Shimono <michael513.sato@gmail.com>
2020-08-03 16:19:14 +0900

    need not connect and end

--- config/passport.js
+++ config/passport.js
@@ -8,9 +8,7 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var dbconfig = require('./database');
 var connection = mysql.createConnection(dbconfig.connection);
-connection.connect();
 connection.query('USE ' + dbconfig.database);
-connection.end();
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
@@ -27,11 +25,9 @@ module.exports = function(passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        connection.connect();
         connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
             done(err, rows[0]);
         });
-        connection.end();
     });
 
     // =========================================================================
@@ -51,7 +47,6 @@ module.exports = function(passport) {
         function(req, username, password, done) {
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            connection.connect();
             connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows) {
                 if (err)
                     return done(err);
@@ -66,16 +61,13 @@ module.exports = function(passport) {
                     };
 
                     var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
-                    connection.connect();
                     connection.query(insertQuery,[newUserMysql.username, newUserMysql.password],function(err, rows) {
                         newUserMysql.id = rows.insertId;
 
                         return done(null, newUserMysql);
                     });
-                    connection.end();
                 }
             });
-            connection.end();
         })
     );
 
@@ -94,7 +86,6 @@ module.exports = function(passport) {
             passReqToCallback : true // allows us to pass back the entire request to the callback
         },
         function(req, username, password, done) { // callback with email and password from our form
-            connection.connect();
             connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
                 if (err)
                     return done(err);
@@ -109,7 +100,6 @@ module.exports = function(passport) {
                 // all is well, return successful user
                 return done(null, rows[0]);
             });
-            connection.end();
         })
     );
 };
